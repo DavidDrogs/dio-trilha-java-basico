765e58eeceb79da8df5e2bba2dd7882be908d516
david pereira <davidjimijuba@gmail.com>
2022-10-14 20:32:05 -0300

    fixando conteudo estudado

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,24 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+         /*tipos, variaveis e constantes  */
+
+         final float PI = 3.14F;
+         final double e;
+ 
+         e = 1.23432;
+ 
+         String nome = "David";
+         String dataAniversario = "10/10/1000";
+         int lote = 35456;
+         boolean aprovado;
+ 
+         aprovado = true;
+ 
+         System.out.println(PI);
+         System.out.println(e);
+         System.out.println(nome);
+         System.out.println(dataAniversario);
+         System.out.println(lote);
+         System.out.println(aprovado);
     }
 }
