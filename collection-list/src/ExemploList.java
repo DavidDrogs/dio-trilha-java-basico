19befa6a8828c744cf7ca066adb780ea5c168013
david pereira <davidjimijuba@gmail.com>
2022-10-28 22:59:09 -0300

    ordenacao de elementos

--- collection-list/src/ExemploList.java
+++ collection-list/src/ExemploList.java
@@ -1,5 +1,105 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
 public class ExemploList {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        //Conhecendo metodos List.
+
+        List<Double> notas = new ArrayList<Double>();
+        notas.add(7.0);
+        notas.add(8.5);
+        notas.add(9.3);
+        notas.add(5.0);
+        notas.add(7.0);
+        notas.add(0.0);
+        notas.add(3.6);
+
+        System.out.println(notas.toString()); //exibe a lista
+        System.out.println("exiba a posicao da nota 5.0: " + notas.indexOf(5d)); //encontra posicao da nota na lista
+
+        System.out.println("Adicione na lista a nota 8.0 na posicao 4: ");
+        notas.add(4, 8d);
+        System.out.println(notas);
+
+        System.out.println("Substitua a nota 5.0 pela nota 6.0: ");
+        notas.set(notas.indexOf(5d), 6.0);
+        System.out.println(notas);
+
+        System.out.println("Confira se a nota 5.0 esta na lista: " + notas.contains(5d));
+
+        System.out.println("Exiba todas as notas na ordem em que foram informadas: ");
+        for(Double nota: notas) System.out.println(nota);
+
+        System.out.println("Exiba a terceira nota adicionada: " + notas.get(2));
+        System.out.println(notas.toString());
+
+        System.out.println("Exiba a menor nota: " + Collections.min(notas));
+
+        System.out.println("Exiba a maior nota: " + Collections.max(notas));
+
+        Iterator<Double> iterator = notas.iterator();
+        Double soma = 0d;
+        while(iterator.hasNext()){
+            Double next = iterator.next();
+            soma += next;
+        }
+        System.out.println("Exiba a soma dos valores: " + soma);
+
+        System.out.println("Exiba a media das notas: " + (soma/notas.size()));
+
+        System.out.println("Remova a nota 0: ");
+        notas.remove(0d);
+        System.out.println(notas);
+
+        System.out.println("Remova a nota da posicao 0: ");
+        notas.remove(0);
+        System.out.println(notas);
+
+        System.out.println("Remova as notas menores que 7 e exiba a lista: ");
+        Iterator<Double> iterator1 = notas.iterator();
+        while(iterator1.hasNext()) {
+            Double next = iterator1.next();
+            if(next < 7) iterator1.remove();
+        }
+        System.out.println(notas);
+
+        System.out.println("Apague toda a lista");
+        notas.clear();
+        System.out.println(notas);
+
+        System.out.println("confira se a lista esta vazia: " + notas.isEmpty());
+
+        //Ordenacao de elementos em uma colecao list.
+
+
+
+
+        
+    }
+}
+
+class Gato {
+    private String nome;
+    private Integer idade;
+    private String cor;
+
+    public Gato(String nome, Integer idade, String cor) {
+        this.nome = nome;
+        this.idade = idade;
+        this.cor = cor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+    public String getCor() {
+        return cor;
     }
 }
